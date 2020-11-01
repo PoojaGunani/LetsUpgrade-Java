e54dbe28d352a7239d4c576d334e9bf83df3c376
PoojaGunani <66996958+PoojaGunani@users.noreply.github.com>
2020-11-01 20:56:07 +0530

    Create Main.java

---
+++ Day8/Q3/Main.java
@@ -0,0 +1,21 @@
+package com.employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	Doctor[] D=new Doctor[3];
+    	Engineer[] E=new Engineer[3];
+    	pilots[] p=new pilots[3];
+    	for(int i=0;i<3; i++) {
+			D[i] = new Doctor();
+			D[i].getDetails();
+			D[i].DocDetail();
+			E[i] = new Engineer();
+			E[i].getDetails();
+			E[i].engDetails();
+			p[i] = new pilots();
+			p[i].getDetails();
+			p[i].pilDetails();
+		}
+    }
+}
