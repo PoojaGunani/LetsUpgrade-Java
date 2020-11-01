747d96a48e74bc107f36c6db41e737affb290ed9
PoojaGunani <66996958+PoojaGunani@users.noreply.github.com>
2020-11-01 20:51:32 +0530

    Create Engineer.java

---
+++ Day8/Q2/Engineer.java
@@ -0,0 +1,9 @@
+package com.employee;
+
+public class Engineer extends parentclass {
+    public void engDetails()
+    {
+        String y="Mr Engineer "+DisplayDetails();
+        System.out.println(y);
+    }
+}
