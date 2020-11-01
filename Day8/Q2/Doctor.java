c42542f8db6b2b2e0dc086bc434b4132f6d17537
PoojaGunani <66996958+PoojaGunani@users.noreply.github.com>
2020-11-01 20:50:44 +0530

    Create Doctor.java

---
+++ Day8/Q2/Doctor.java
@@ -0,0 +1,9 @@
+package com.employee;
+
+public class Doctor extends parentclass {
+    public void DocDetail()
+    {
+        String y="Doctor "+DisplayDetails();
+        System.out.println(y);
+    }
+}
