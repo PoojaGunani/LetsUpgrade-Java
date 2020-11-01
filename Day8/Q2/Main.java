13084c7e76d4802817b5760e9cbe1801e29ffd78
PoojaGunani <66996958+PoojaGunani@users.noreply.github.com>
2020-11-01 20:52:40 +0530

    Create Main.java

---
+++ Day8/Q2/Main.java
@@ -0,0 +1,16 @@
+package com.employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Doctor D=new Doctor();
+	    D.getDetails();
+	    D.DocDetail();
+	    Engineer E=new Engineer();
+	    E.getDetails();
+	    E.engDetails();
+	    pilots p=new pilots();
+	    p.getDetails();
+	    p.pilDetails();
+    }
+}
